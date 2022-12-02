15634ef9956d277919560f723ceba07b898f2c5d
Vibha Chandrala <Vibha Chandrala@192.168.0.202>
2022-12-02 10:07:35 +0530

    Build2

---
+++ src/main/java/scripts/Test1.java
@@ -0,0 +1,14 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void tc1() {
+		Reporter.log("Hello java",true);
+	}
+	
+
+}
---
+++ src/main/java/scripts/Test4.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test4 {
+
+	@Test
+	public void tc4() {
+		Reporter.log("Hello python",true);
+	}
+}
